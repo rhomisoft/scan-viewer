80f7a587ca39bbd271e29e56071d5b42d8bb0b41
eboturkson <eboturkson@nims.edu.gh>
2015-12-06 15:28:50 +0000

    Change drive credentials

--- scripts/app-811b2eb1.js
+++ scripts/app-811b2eb1.js
@@ -88,7 +88,7 @@ var module=angular.module("editor.drive",["editor.gapi"]);module.service("drive"
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-angular.module("editor",["editor.login","editor.rename","editor.drive","ngRoute","ngMaterial","ui.codemirror"]).constant("apiKey",null).constant("clientId","709207149709-fadikftqudacphtr4pq5mu80s6tqklrb.apps.googleusercontent.com").constant("applicationId","709207149709").constant("scope",["email","profile","https://www.googleapis.com/auth/drive","https://www.googleapis.com/auth/drive.install"]).constant("loadApis",{drive:"v2"}).config(["$routeProvider",function(e){e.when("/edit/:fileId?",{templateUrl:"app/main/main.html",controller:"MainCtrl",controllerAs:"ctrl"}).otherwise({redirectTo:function(){return console.log("Otherwise..."),"/edit/"}})}]);/*
+angular.module("editor",["editor.login","editor.rename","editor.drive","ngRoute","ngMaterial","ui.codemirror"]).constant("apiKey",null).constant("clientId","918273953019-mgg5h4vhpctvra3ccmmd3m746f1q5tos.apps.googleusercontent.com").constant("applicationId","918273953019").constant("scope",["email","profile","https://www.googleapis.com/auth/drive","https://www.googleapis.com/auth/drive.install"]).constant("loadApis",{drive:"v2"}).config(["$routeProvider",function(e){e.when("/edit/:fileId?",{templateUrl:"app/main/main.html",controller:"MainCtrl",controllerAs:"ctrl"}).otherwise({redirectTo:function(){return console.log("Otherwise..."),"/edit/"}})}]);/*
  * Copyright 2015 Google Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
